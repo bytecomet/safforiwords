853b5464c3d1b6751fb6b7103752b31a9cbcaf39
bytecomet <70172430+bytecomet@users.noreply.github.com>
2026-03-03 20:32:25 -0500

    Change target word selection to random from list
    Replaced target word selection method with random choice.

--- script.js
+++ script.js
@@ -1133,8 +1133,10 @@ const alertContainer = document.querySelector("[data-alert-container]")
 const guessGrid = document.querySelector("[data-guess-grid]")
 const offsetFromDate = new Date(2022, 0, 1)
 const msOffset = Date.now() - offsetFromDate
-const dayOffset = msOffset / 1000 / 60 / 60 / 24
-const targetWord = targetWords[Math.floor(dayOffset)]
+//const dayOffset = msOffset / 1000 / 60 / 60 / 24
+//const targetWord = targetWords[Math.floor(dayOffset)]
+const targetWord = targetWords[Math.floor(Math.random() * targetWords.length)]
+console.log(targetWord)
 
 startInteraction()
 
